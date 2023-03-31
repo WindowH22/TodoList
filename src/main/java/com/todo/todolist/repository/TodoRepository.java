ca021313ef32030eed837c6d08376fbca31cb21d
WindowH22 <dlckdgml1235@naver.com>
2023-03-31 20:53:45 +0900

    feat: TodoRepository 구현

---
+++ src/main/java/com/todo/todolist/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.todo.todolist.repository;
+
+import com.todo.todolist.domain.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
